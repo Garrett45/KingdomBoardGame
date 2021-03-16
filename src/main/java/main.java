91f07bc794b2314cd2d047df7844cca5de8e50c9
Grant James Udstrand <grant.udstrand@gmail.com>
2021-03-16 07:55:34 -0500

    Stable Gradle Files. This should give us a better place for our sources files now.

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+//        Hex[] hexes = new Hex[10];
+        GameBoard thing = new GameBoard();
+        System.out.print("k\n");
+    }
+}
---
+++ src/test/java/mainTest.java
@@ -0,0 +1,14 @@
+//import java.io.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@Test
+//class mainTest {
+//    public void testMain() throws IOException, FileNotFoundException {
+//        String[] args = null;
+//        final InputStream original = System.in;
+//        final FileInputStream fips = new FileInputStream(new File("test_file"));
+//        System.setIn(fips);
+//        Main.main(args);
+//        System.setIn(original);
+//    }
+//}
